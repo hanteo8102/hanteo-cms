f10a61779d8e1d27f11c233f5ea2afd0addd0603
Kentaro <Kentaro@kakao.com>
2022-02-09 16:43:18 +0900

    사이트 레이아웃 1차 번역 완료

---
+++ admin/src/translations/index.js
@@ -0,0 +1,14 @@
+import en from './en.json';
+import ko from './ko.json';
+
+const trads = {
+  en,
+  ko,
+};
+
+export const languageNativeNames = {
+  en: 'English',
+  ko: '한국어',
+};
+
+export default trads;
---
+++ extensions/upload/admin/src/translations/index.js
@@ -0,0 +1,9 @@
+import en from './en.json';
+import ko from './ko.json';
+
+const trads = {
+  en,
+  ko,
+};
+
+export default trads;

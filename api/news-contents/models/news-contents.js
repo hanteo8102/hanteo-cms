55769a52377df1d94f48469c4f5f99eef5806ac9
Kentaro <Kentaro@kakao.com>
2022-05-31 10:23:59 +0900

    번역 일부 수정

--- api/news-contents/models/news-contents.js
+++ api/news-contents/models/news-contents.js
@@ -1,8 +1,20 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    // Called before an entry is created
+    beforeCreate(data) {
+      const { contents } = data
+      const img = contents.match(/<img[^>]+src="http([^">]+)/g)
+      if (img) {
+        const url = img[0].split('"')
+        data.thumbnail_path = url[1]
+      }
+    },
+  },
+}

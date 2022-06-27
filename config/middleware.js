eec2ffbb843ad8fbf14549700ee60f8dc641b3e8
Kentaro <Kentaro@kakao.com>
2022-06-27 23:30:57 +0900

    # 미들웨어 헤더 수정

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: ['https://hanteo.ga', 'https://api.hanteo.ga'],
+      origin: ['*'],
       headers: ['*'],
     },
   },

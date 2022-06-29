65f7f71e83230c140960968eb035934acd661c16
Kentaro <Kentaro@kakao.com>
2022-06-29 15:19:59 +0900

    # 미들웨어 헤더 수정

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,25 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: env(
-        'CORS_ORIGIN',
-        'http://localhost:3000,http://localhost:1337'
-      ).split(','),
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      settings: {
+        cors: {
+          enabled: true,
+          origin: env(
+            'CORS_ORIGIN',
+            'http://localhost:3000,http://localhost:1337'
+          ).split(','),
+          headers: ['*'],
+        },
+      },
+    }
+  }
+  return {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ['*'],
+        headers: ['*'],
+      },
     },
-  },
-})
+  }
+}

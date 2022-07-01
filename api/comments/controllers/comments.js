294dd66a56365432d3c877389fd621c51baa520a
Kentaro <Kentaro@kakao.com>
2022-07-01 16:29:40 +0900

    # 일부 로직 수정

--- api/comments/controllers/comments.js
+++ api/comments/controllers/comments.js
@@ -16,39 +16,52 @@ module.exports = {
 
   async find(ctx) {
     let query = ''
+    let writerQuery = ''
     const type = ctx.query.type
     const id = ctx.query.type_id
+    const writer = ctx.query.writer
 
     if (type && id) {
       query = `AND comments.type = '${type}' AND comments.type_id = ${id}`
     }
 
+    if (writer) {
+      writerQuery = `AND comments.writer = '${writer}'`
+    }
+
     let sql = `
-      select comments.*,
-             (select count(1)
-              from article_elements
-              where comments.id = article_elements.type_id
+      SELECT comments.*,
+             (SELECT count(1)
+              FROM article_elements
+              WHERE comments.id = article_elements.type_id
                 AND type = 'comment'
-                AND expression = true)  AS like_count,
-             (select count(1)
-              from article_elements
-              where comments.id = article_elements.type_id
+                AND good = true)                       AS good_count,
+             (SELECT count(1)
+              FROM article_elements
+              WHERE comments.id = article_elements.type_id
                 AND type = 'comment'
-                AND expression = false) AS hate_count,
-             (select count(1)
-              from re_comments
-              where comments.id = re_comments.comment) AS re_comment_count,
-             U.nick_name
-      from comments
-             INNER JOIN "users-permissions_user" AS U ON (comments.user = U.id)
-      WHERE comments.is_delete = false ${query}
+                AND hate = false)                      AS hate_count,
+             (SELECT count(1)
+              FROM re_comments
+              WHERE comments.id = re_comments.comment) AS re_comment_count,
+             U.nick_name,
+             CASE
+               WHEN comments.type = 'news' then 0
+               WHEN comments.type = 'board'
+                 THEN (SELECT category
+                       FROM boards st1
+                       WHERE st1.id = comments.type_id)
+               end                                     AS category
+      FROM comments
+             INNER JOIN "users-permissions_user" AS U ON (comments.writer = U.id)
+      WHERE comments.is_delete = false ${query} ${writerQuery}
       ORDER BY comments.created_at
     `
 
     let result = await strapi.connections.default.raw(sql)
 
     return result.rows.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models['boards'] })
+      sanitizeEntity(entity, { model: strapi.models['article-elements'] })
     )
   },
 }

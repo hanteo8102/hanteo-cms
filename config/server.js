a9bbc1db30f8b509067b83de0b4ff54a42cc2038
Kentaro <Kentaro@kakao.com>
2022-07-04 02:43:36 +0900

    # 서버 js 변경

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'http://localhost:1337'),
+  url: env('SERVER_URL'),
   admin: {
-    url: env('ADMIN_URL', 'http://localhost:1337/admin'),
+    url: env('ADMIN_URL'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4f120c167a62817338b16b580a61b44b'),
     },

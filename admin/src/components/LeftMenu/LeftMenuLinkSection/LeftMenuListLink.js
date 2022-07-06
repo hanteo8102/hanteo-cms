89335adfb5996743c1adc58e6b86b18ec31d44b5
Kentaro <Kentaro@kakao.com>
2022-07-06 21:12:15 +0900

    왼쪽메뉴 스크롤바 제거

--- admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
+++ admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const LeftMenuListLink = styled.div`
   max-height: ${({ section }) =>
-    section === 'collectionType' ? '400px' : '180px'};
+    section === 'collectionType' ? '1000px' : '180px'};
   margin-bottom: 19px;
   margin-right: 25px;
   overflow: auto;

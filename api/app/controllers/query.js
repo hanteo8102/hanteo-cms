2221a9bd81caa084bad4df05bbe72fb0f4bd0306
Kentaro <Kentaro@kakao.com>
2022-08-01 15:15:44 +0900

    # Count 일부 수정

--- api/app/controllers/query.js
+++ api/app/controllers/query.js
@@ -200,36 +200,37 @@ const querySelectorUserContentsComment = (user) => {
 
 const handleCountUserContentsComment = (user) => {
   return `
-    SELECT COUNT(*),
-           (SELECT COUNT(1)
-            FROM article_elements
-            WHERE comments.id = article_elements.type_id
-              AND type = 'comment'
-              AND good = true)                       AS good_count,
-           (SELECT COUNT(1)
-            FROM article_elements
-            WHERE comments.id = article_elements.type_id
-              AND type = 'comment'
-              AND hate = false)                      AS hate_count,
-           (SELECT COUNT(1)
-            FROM re_comments
-            WHERE comments.id = re_comments.comment) AS re_comment_count,
-           U.nick_name,
-           CASE
-             WHEN comments.type = 'news' then 0
-             WHEN comments.type = 'board'
-               THEN (SELECT category
-                     FROM boards st1
-                     WHERE st1.id = comments.type_id)
-             END                                     AS category,
-           (SELECT CAST(count(1) AS INT)
-            FROM comments
-            WHERE comments.is_delete = false
-              AND comments.writer = ${user})         AS total_count
-    FROM comments
-           INNER JOIN "users-permissions_user" AS U ON (comments.writer = U.id)
-    WHERE comments.is_delete = false
-      AND comments.writer = ${user}
+    SELECT COUNT(*) FROM
+      (SELECT comments.*,
+              (SELECT count(1)
+               FROM article_elements
+               WHERE comments.id = article_elements.type_id
+                 AND type = 'comment'
+                 AND good = true)                       AS good_count,
+              (SELECT count(1)
+               FROM article_elements
+               WHERE comments.id = article_elements.type_id
+                 AND type = 'comment'
+                 AND hate = false)                      AS hate_count,
+              (SELECT count(1)
+               FROM re_comments
+               WHERE comments.id = re_comments.comment) AS re_comment_count,
+              U.nick_name,
+              CASE
+                WHEN comments.type = 'news' then 0
+                WHEN comments.type = 'board'
+                  THEN (SELECT category
+                        FROM boards st1
+                        WHERE st1.id = comments.type_id)
+                END                                     AS category,
+              (SELECT CAST(count(1) AS INT)
+               FROM comments
+               WHERE comments.is_delete = false
+                 AND comments.writer = ${user})       AS total_count
+       FROM comments
+              INNER JOIN "users-permissions_user" AS U ON (comments.writer = U.id)
+       WHERE comments.is_delete = false
+         AND comments.writer = ${user}) AS a
   `
 }
 

b50d762540a0285bf29ed0ec43312594844d3097
pyong91 <rushhour9@naver.com>
2022-09-03 19:50:00 +0900

    # 신고하기 create 추가

--- api/complain-history/controllers/complain-history.js
+++ api/complain-history/controllers/complain-history.js
@@ -5,4 +5,37 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+
+    const query = {
+      _where: [
+        {type: ctx.request.body.type},
+        {type_id: ctx.request.body.type_id}
+      ]
+    }
+    let complainsEntity = await strapi.services.complain.find(query)
+
+    if(complainsEntity.length === 0) {
+      complainsEntity = await strapi.services.complain.create({
+        type: ctx.request.body.type,
+        type_id: ctx.request.body.type_id,
+        writer: ctx.request.body.writer,
+        contents: ctx.request.body.contents,
+      })
+    } else {
+      complainsEntity = complainsEntity[0]
+    }
+
+    const entity = await strapi.services['complain-history'].create({...ctx.request.body, complain: complainsEntity.id});
+    return sanitizeEntity(entity, { model: strapi.models['complain-history'] });
+  },
+};

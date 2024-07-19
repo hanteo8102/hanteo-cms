318b49a08be1b693255e8bd62fec4c0d56cd2554
한터 <hanteo@hanteoui-MacBookPro.local>
2024-07-20 01:16:53 +0900

    # 메인 게시판 일부 수정

--- api/app/controllers/query.js
+++ api/app/controllers/query.js
@@ -20,119 +20,121 @@ const querySelectorBoard = (category, query) => {
   return `
     select a.*
     from (select boards.id,
-                 boards.title,
-                 boards.contents,
-                 boards.writer,
-                 boards.is_delete,
-                 boards.category,
-                 boards.writing_type,
-                 boards.color_type,
-                 boards.view_count,
-                 boards.created_by,
-                 boards.updated_by,
-                 boards.created_at,
-                 boards.updated_at,
-                 0 < (SELECT COUNT(*)
-                      FROM block_user_lists
-                      WHERE user_id = ${user}
-                        AND boards.writer = block_user_id) AS isBlock,
-                 (select count(1)
-                  from article_elements
-                  inner join "users-permissions_user" AS U ON U.id = article_elements.writer
-                  where boards.id = article_elements.type_id
-                    AND type = 'board'
-                    AND good = true)                       AS good_count,
-                 (select count(1)
-                  from article_elements
-                  inner join "users-permissions_user" AS U ON U.id = article_elements.writer
-                  where boards.id = article_elements.type_id
-                    AND type = 'board'
-                    AND hate = true)                       AS hate_count,
-                 (select count(1)
-                  from comments
-                  inner join "users-permissions_user" AS U ON U.id = comments.writer
-                  where boards.id = comments.type_id
-                    AND comments.is_delete = false
-                    AND type = 'board')                    AS comment_count,
-                 (select count(1)
-                  from re_comments
-                  inner join "users-permissions_user" AS U ON U.id = re_comments.writer
-                  where boards.id = re_comments.type_id
-                    AND type = 'board'
-                    AND re_comments.is_delete = false)                    AS re_comment_count,
-                 U.nick_name
-          from boards
-                 INNER JOIN "users-permissions_user" AS U ON (boards.writer = U.id)
-          WHERE boards.is_delete = false
-            AND boards.writing_type != N'일반 게시물'
-            AND boards.category = ${category}
-          ORDER BY boards.created_at DESC
-          OFFSET 0 LIMIT 5) as a
+    boards.title,
+    boards.contents,
+    boards.writer,
+    boards.is_delete,
+    boards.category,
+    boards.writing_type,
+    boards.color_type,
+    boards.view_count,
+    boards.created_by,
+    boards.updated_by,
+    boards.created_at,
+    boards.updated_at,
+    boards.board_expired_date,
+    0 < (SELECT COUNT(*)
+      FROM block_user_lists
+      WHERE user_id = ${user}
+        AND boards.writer = block_user_id) AS isBlock,
+    (select count(1)
+    from article_elements
+    inner join "users-permissions_user" AS U ON U.id = article_elements.writer
+    where boards.id = article_elements.type_id
+    AND type = 'board'
+    AND good = true)                       AS good_count,
+    (select count(1)
+    from article_elements
+    inner join "users-permissions_user" AS U ON U.id = article_elements.writer
+    where boards.id = article_elements.type_id
+    AND type = 'board'
+    AND hate = true)                       AS hate_count,
+    (select count(1)
+    from comments
+    inner join "users-permissions_user" AS U ON U.id = comments.writer
+    where boards.id = comments.type_id
+    AND comments.is_delete = false
+    AND type = 'board')                    AS comment_count,
+    (select count(1)
+    from re_comments
+    inner join "users-permissions_user" AS U ON U.id = re_comments.writer
+    where boards.id = re_comments.type_id
+    AND type = 'board'
+    AND re_comments.is_delete = false)                    AS re_comment_count,
+    U.nick_name
+    from boards
+    INNER JOIN "users-permissions_user" AS U ON (boards.writer = U.id)
+    WHERE boards.is_delete = false
+    AND boards.writing_type != N'일반 게시물'
+    AND boards.category = ${category}
+    ORDER BY boards.created_at DESC
+    OFFSET 0 LIMIT 5) as a
     UNION ALL
     select b.*
     from (select boards.id,
-                 CASE
-                   WHEN 0 < (SELECT COUNT(*)
-                             FROM block_user_lists
-                             WHERE user_id = ${user}
-                               AND boards.writer = block_user_id)
-                     THEN N'차단된 멤버의 게시글입니다.'
-                   ELSE boards.title
-                   END                                     AS title,
-                 CASE
-                   WHEN 0 < (SELECT COUNT(*)
-                             FROM block_user_lists
-                             WHERE user_id = ${user}
-                               AND boards.writer = block_user_id)
-                     THEN ''
-                   ELSE boards.contents
-                   END                                     AS contents,
-                 boards.writer,
-                 boards.is_delete,
-                 boards.category,
-                 boards.writing_type,
-                 boards.color_type,
-                 boards.view_count,
-                 boards.created_by,
-                 boards.updated_by,
-                 boards.created_at,
-                 boards.updated_at,
-                 0 < (SELECT COUNT(*)
-                      FROM block_user_lists
-                      WHERE user_id = ${user}
-                        AND boards.writer = block_user_id) AS isBlock,
-                 (select count(1)
-                  from article_elements
-                  inner join "users-permissions_user" AS U ON U.id = article_elements.writer
-                  where boards.id = article_elements.type_id
-                    AND type = 'board'
-                    AND good = true)                       AS good_count,
-                 (select count(1)
-                  from article_elements
-                  inner join "users-permissions_user" AS U ON U.id = article_elements.writer
-                  where boards.id = article_elements.type_id
-                    AND type = 'board'
-                    AND hate = true)                       AS hate_count,
-                 (select count(1)
-                  from comments
-                  inner join "users-permissions_user" AS U ON U.id = comments.writer
-                  where boards.id = comments.type_id
-                    AND comments.is_delete = false
-                    AND type = 'board')                    AS comment_count,
-                 (select count(1)
-                  from re_comments
-                  inner join "users-permissions_user" AS U ON U.id = re_comments.writer
-                  where boards.id = re_comments.type_id
-                    AND type = 'board'
-                    AND re_comments.is_delete = false)                    AS re_comment_count,
-                 U.nick_name
-          from boards
-                 INNER JOIN "users-permissions_user" AS U ON (boards.writer = U.id)
-          WHERE boards.is_delete = false
-            AND boards.writing_type = N'일반 게시물'
-            AND boards.category = ${category}
-          ORDER BY boards.created_at DESC
-          OFFSET ${start} LIMIT ${limit}) as b
+    CASE
+    WHEN 0 < (SELECT COUNT(*)
+              FROM block_user_lists
+              WHERE user_id = ${user}
+                AND boards.writer = block_user_id)
+      THEN N'차단된 멤버의 게시글입니다.'
+    ELSE boards.title
+    END                                     AS title,
+    CASE
+    WHEN 0 < (SELECT COUNT(*)
+              FROM block_user_lists
+              WHERE user_id = ${user}
+                AND boards.writer = block_user_id)
+      THEN ''
+    ELSE boards.contents
+    END                                     AS contents,
+    boards.writer,
+    boards.is_delete,
+    boards.category,
+    boards.writing_type,
+    boards.color_type,
+    boards.view_count,
+    boards.created_by,
+    boards.updated_by,
+    boards.created_at,
+    boards.updated_at,
+    boards.board_expired_date,
+    0 < (SELECT COUNT(*)
+      FROM block_user_lists
+      WHERE user_id = ${user}
+        AND boards.writer = block_user_id) AS isBlock,
+    (select count(1)
+    from article_elements
+    inner join "users-permissions_user" AS U ON U.id = article_elements.writer
+    where boards.id = article_elements.type_id
+    AND type = 'board'
+    AND good = true) AS good_count,
+    (select count(1)
+    from article_elements
+    inner join "users-permissions_user" AS U ON U.id = article_elements.writer
+    where boards.id = article_elements.type_id
+    AND type = 'board'
+    AND hate = true) AS hate_count,
+    (select count(1)
+    from comments
+    inner join "users-permissions_user" AS U ON U.id = comments.writer
+    where boards.id = comments.type_id
+    AND comments.is_delete = false
+    AND type = 'board')  AS comment_count,
+    (select count(1)
+    from re_comments
+    inner join "users-permissions_user" AS U ON U.id = re_comments.writer
+    where boards.id = re_comments.type_id
+    AND type = 'board'
+    AND re_comments.is_delete = false)  AS re_comment_count,
+    U.nick_name
+    from boards
+    INNER JOIN "users-permissions_user" AS U ON (boards.writer = U.id)
+    WHERE boards.is_delete = false
+    AND boards.writing_type = N'일반 게시물'
+    AND boards.category = ${category}
+    ORDER BY boards.created_at DESC
+    OFFSET ${start} LIMIT ${limit}) as b
   `
 }
 
